e95a77d56952a2093b980279314c1d935fbbe7a4
marco_m <kolkonos@hotmail.com>
2024-11-08 09:40:36 -0700

    Add project files.

---
+++ MainPage2.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MicaBackdropMAUI
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+            menu_switch.IsToggled = Shell.Current.FlyoutBehavior == FlyoutBehavior.Locked;
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            Shell.Current.FlyoutBehavior = e.Value ? FlyoutBehavior.Locked : FlyoutBehavior.Flyout;
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace MicaBackdropMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(lifecycle => {
+                lifecycle.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated((window) =>
+                    {
+                        window.ExtendsContentIntoTitleBar = true;
+                        window.SystemBackdrop = new Microsoft.UI.Xaml.Media.MicaBackdrop() { Kind = Microsoft.UI.Composition.SystemBackdrops.MicaKind.BaseAlt };
+
+                        //var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        //var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        //var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        //switch (appWindow.Presenter)
+                        //{
+                        //    case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                        //        overlappedPresenter.Maximize();
+                        //        break;
+                        //}
+                    });
+                });
+
+            });
+#endif
+
+            return builder.Build();
+        }
+    }
+}
