b4cdccaedcced5086d72a2fe1da63586cb219749
marco_m <kolkonos@hotmail.com>
2024-11-12 10:45:26 -0700

    Completed solution that attains the fully transparent navigation view that is used in the Windows 11 Settings app.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,36 @@
-﻿namespace MicaBackdropMAUI
+﻿using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Platform;
+#if WINDOWS
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace MicaBackdropMAUI;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        this.Loaded += (_, _) => {
+            OverrideBg();
+        };
+    }
+
+    internal void OverrideBg()
+    {
+#if WINDOWS        
+        var shellView = Shell.Current?.Handler?.PlatformView as ShellView;
+        var navigationView = shellView?.Content as MauiNavigationView;
+
+        var contentGrid = navigationView?
+            .GetType()
+            .GetProperty("ContentGrid", 
+            System.Reflection.BindingFlags.NonPublic | 
+                      System.Reflection.BindingFlags.Instance)?
+            .GetValue(navigationView) as Microsoft.UI.Xaml.Controls.Grid;
+
+        contentGrid!.Background.Opacity = 0;
+#endif
     }
 }
